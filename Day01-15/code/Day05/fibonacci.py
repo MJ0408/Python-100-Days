a4bb3e1555b6b51aa298d12843fb6a7cde9ba927
MJ0408 <ymj0222@163.com>
2023-06-01 18:55:54 +0800

    init

--- Day01-15/code/Day05/fibonacci.py
+++ Day01-15/code/Day05/fibonacci.py
@@ -6,6 +6,16 @@ Version: 0.1
 Author: 骆昊
 Date: 2018-03-02
 """
+a= 0
+b= 1
+print(b,end=' ')
+for i in range(19):
+    c = a + b
+    print(c,end=' ')
+    a = b
+    b = c
+print('\n')
+
 
 a = 0
 b = 1
